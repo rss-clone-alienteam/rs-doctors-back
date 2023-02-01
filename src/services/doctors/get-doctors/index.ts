4e1bebcc5bb686ca0bec6a6ebdaf39e680d9773e
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-02-01 10:40:13 +0100

    feat: add eslint

--- src/services/doctors/get-doctors/index.ts
+++ src/services/doctors/get-doctors/index.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-export const handler: APIGatewayProxyHandler = async (event) => {
+export const handler: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify([1, 2, 3]),
