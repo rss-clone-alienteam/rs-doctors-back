4c8487462e6231b54dad01d1ed2e051f2230d300
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-02-08 15:19:52 +0100

    feat: add lambda-function get-doctors

--- src/services/doctors/get-doctors/index.ts
+++ src/services/doctors/get-doctors/index.ts
@@ -1,8 +1,27 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { ScanCommand } from '@aws-sdk/client-dynamodb';
+import { ddbDocClient } from 'src/utils/aws/dynamo';
+import { getEnvVariable } from 'src/utils/runtime';
 
-export const handler: APIGatewayProxyHandler = async () => {
+const DOCTORS_TABLE = getEnvVariable('DOCTORS_TABLE');
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const category = event.queryStringParameters ? event.queryStringParameters.category : '';
+  const city = event.queryStringParameters ? event.queryStringParameters.city : '';
+
+  const params = {
+    TableName: DOCTORS_TABLE,
+    ExpressionAttributeValues: {
+      ':category': { S: `${category}` },
+      ':city': { S: `${city}` },
+    },
+    FilterExpression: 'category = :category and city = :city',
+  };
+
+  const data = await ddbDocClient.send(new ScanCommand(params));
+
   return {
     statusCode: 200,
-    body: JSON.stringify(['1', '2', '3']),
+    body: JSON.stringify(data),
   };
 };
