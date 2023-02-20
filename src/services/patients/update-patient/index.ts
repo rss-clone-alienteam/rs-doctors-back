f1c338e536e076da3b519259a0fe7a29ae77a953
sharpman13 <sharpman2001@gmail.com>
2023-02-20 16:25:41 +0100

    refactor: update-patient

--- src/services/patients/update-patient/index.ts
+++ src/services/patients/update-patient/index.ts
@@ -1,20 +1,24 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { getBody } from 'src/utils/aws/lambda';
-import { UpdateCommand  } from '@aws-sdk/lib-dynamodb';
-import { ddbDocClient, getUpdateExpression, getUpdateValues } from 'src/utils/aws/dynamo';
+import { UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  ddbDocClient,
+  getUpdateExpression,
+  getUpdateValues,
+} from 'src/utils/aws/dynamo';
 import { getEnvVariable } from 'src/utils/runtime';
 import { Appointment } from 'src/types/IPatient';
 
-interface IDataUpdate {
-  appointments?: Appointment
-}
+// interface IDataUpdate {
+//   appointments?: Appointment
+// }
 
 const PATIENTS_TABLE = getEnvVariable('PATIENTS_TABLE');
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const id = event?.pathParameters?.id;
 
-  const updateInfo = getBody<IDataUpdate>(event);
+  const updateInfo = getBody<Appointment>(event);
 
   const UpdateExpression = getUpdateExpression(updateInfo);
   const ExpressionAttributeValues = getUpdateValues(updateInfo);
--- src/types/IPatient.ts
+++ src/types/IPatient.ts
@@ -10,5 +10,5 @@ export default interface IPatient {
   city: string;
   email: string;
   password: string;
-  appointments: Appointment[];
+  appointments: Array<Appointment>;
 }
